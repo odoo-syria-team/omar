b85233bbfa03c3ef567b0ed7cb39a6371dfea480
odoo-syria-team <153421050+odoo-syria-team@users.noreply.github.com>
2024-01-10 12:38:08 +0300

    Add files via upload

--- Change_Currency/models/sale_order.py
+++ Change_Currency/models/sale_order.py
@@ -2,19 +2,18 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from odoo import api, fields, models, _
-from .log import logger
 
 
 class SaleOrder(models.Model):
     _inherit = "res.company"
 
     test = fields.Boolean('test', compute="teeeest")
+    many2many_test = fields.Many2many('account.move.line', 'many2many_test')
 
     def teeeest(self):
         for rec in self:
             rec.test = False
             account_move_lines = self.env['account.move.line'].search([('company_id', '=', rec.id)])
 
-            logger.debug(account_move_lines)
             for re in account_move_lines:
                 re.company_currency_id = None
